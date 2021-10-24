8403bcac5e203b7ad236a244c8c20b98d6f1c60b
Judit Meaca <juditmeaca@gmail.com>
2021-10-24 13:59:58 -0300

    Agregue nuevas imagenes, header y footer

--- js/obstacle.js
+++ js/obstacle.js
@@ -6,11 +6,12 @@ class Obstacle extends Element{
     }
     addMe(){
         let obs=document.createElement("div");
-        obs.classList.add("asteroid");
+        obs.classList.add("blackhole");
         obs.classList.add("pos"+this.pos);
         obs.setAttribute("id",this.name);
         let containGame=document.getElementById("stars");
         containGame.appendChild(obs);
+
     }
     getPos(){
         let me=document.getElementById(this.name);
